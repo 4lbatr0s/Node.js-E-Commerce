6fdd1c603769916c61ba63abadc72b85c665e5e0
serhat0ner <serhatoner@protonmail.com>
2022-11-13 15:36:40 +0300

    persistance for user & cart slices are  created

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import {RouterProvider } from 'react-router-dom';
 import Routes from './routes'; 
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react' //INFO: you should install redux-persist library!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}> {/*INFO: How to use Redux in the project*/}
-    <RouterProvider router = {Routes}/>
+    <PersistGate loading={null} persistor={persistor}> {/*INFO: persist should become after Provider */}
+      <RouterProvider router = {Routes}/>
+    </PersistGate>
   </Provider>
 );
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux"; //INFO: We basically imported cartSlice.reducer value
 import userReducer from "./userRedux";
+import { 
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'; //INFO: How to import persistance.
+  import storage from 'redux-persist/lib/storage'
 
-export default configureStore({
-    reducer:{
-        cart:cartReducer,
-        user:userReducer,
-    }
+
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const rootReducer = combineReducers({user:userReducer, cart:cartReducer}); //INFO: How to combine reducers.
+
+const persistedReducer = persistReducer(persistConfig, rootReducer); //TIP: persist userReducer, in this way user info in the userRedux does not go away.
+
+export const store = configureStore({
+    reducer:persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+
+export const persistor = persistStore(store);  //INFO: How to export persistStores, after this go to index.js and provide PersistGate
