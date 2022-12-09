f0d79f722b6b9a111f9e720d91a5213bd3da33c0
4lbatr0s <serhatoner@protonmail.com>
2022-12-09 17:58:13 +0300

    change access token processing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -111,12 +111,17 @@ function Navbar() {
                 <Logo>VERITAS.</Logo>
             </Center>
             <Right>
-                <MenuItem>
-                    REGISTER
-                </MenuItem>
-                <MenuItem>
-                    SIGN IN
-                </MenuItem>   
+            <Link to="/register">
+                    <MenuItem>
+                        REGISTER
+                    </MenuItem>       
+                </Link>
+
+                <Link to="/login">
+                    <MenuItem>
+                        LOG IN
+                    </MenuItem>       
+                </Link>
                 <Link to="/cart">  {/**TIP: How to go cart pages */}
                     <MenuItem>
                     <Badge badgeContent={quantity} color="primary">
--- src/helpers/axios_helper.js
+++ src/helpers/axios_helper.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { TEST_URL } from "./url_helper";
 
 const BASE_URL = TEST_URL;
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNTk4OThmMTU1NTc4OTRiNGJjMWYzMiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2ODI4NTEwNywiZXhwIjoxNjY4NTQ0MzA3fQ.uLxk5AVj1ArKPWrri7BTuxlf89vsckSZY2kC7djB-v0"
+
+//INFO: HOW TO DYNAMICALLY GET ACCESSTOKEN WITH REDUX-PERSIST!
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).currentUser).accessToken; //TIP: root: go to store and see key  value of the persistConfig. It's root! 
 
 
 //INFO: How to create an axios 
